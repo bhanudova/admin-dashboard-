40416585874f48b40479bb0579f50c3151ea525a
Bhanu_Dova <130201202+bhanudova@users.noreply.github.com>
2024-05-20 09:48:08 +0530

    ui changes added

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -3,23 +3,35 @@ import { LuArrowLeftToLine } from "react-icons/lu";
 
 export default function Admin() {
   return (
-    <div className="relative h-full">
-      {/* <div className=" h-[92%] ">
+
+    <div className='relative h-full' >
+      <div className='w-full py-6 mt-1 space-y-6 px-2 '>
         <div className="grid grid-cols-3 gap-3">
-          <div className="p-20 bg-blue-200 rounded-lg">
+          <div className="p-20 bg-blue-50 rounded-lg">
 
           </div>
-          <div className="p-20 bg-yellow-200 rounded-lg">
+          <div className="p-20 bg-yellow-50 rounded-lg">
 
           </div>
-          <div className="p-20 bg-red-200 rounded-lg">
+          <div className="p-20 bg-red-50 rounded-lg">
 
           </div>
         </div>
+
+        <div className="grid grid-cols-2 gap-3">
+          <div className="p-24 bg-green-50 rounded-lg">
+
+          </div>
+          <div className="p-24 bg-lime-50 rounded-lg">
+
+          </div>
+        </div>
+
+
       </div>
-      <div className='flex items-center justify-center h-[8%] border-t border-b w-full p-2 absolute bottom-0'>
-        <LuArrowLeftToLine /><h2 className='text-sm font-bold'>Collapsed View</h2>
-      </div> */}
+      <div className='flex items-center h-[8%] justify-center border-t border-b w-full p-2 absolute bottom-0'>
+        
+      </div>
     </div>
   );
 }
