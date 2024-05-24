dcfbbc0bef0f866778b3f88071c29addaddf7d46
Bhanu_Dova <130201202+bhanudova@users.noreply.github.com>
2024-05-24 10:13:52 +0530

    ui changes added

--- app/components/SignInComponent.tsx
+++ app/components/SignInComponent.tsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link';
+import Image from 'next/image';
 const SignInComponent = () => {
     const router = useRouter();
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <form className='shadow-md shadow-green-600 p-10 rounded-lg w-[30%] mx-auto'>
-                <h2 className='text-center font-semibold text-2xl mb-4'>Sign Page</h2>
+        <div className='flex justify-center relative items-center h-screen'>
+            <Image src={'/signin_bg.jpg'} width={1000} height={1000} alt='' className='w-full h-full '/>
+            <form className=' bg-white  absolute p-10 rounded-lg w-[30%] mx-auto'>
+                <h2 className='text-center font-semibold text-2xl mb-4'>Sign In</h2>
                 <dl>
                     <dt className='font-semibold'>User Name</dt>
                     <dd className='mt-1'><input value={'demo@gmail.com'} type="text" className='border-2 border-green-200 p-1.5 rounded-md focus:outline-none w-full' /></dd>
